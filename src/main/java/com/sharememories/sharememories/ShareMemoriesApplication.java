30166320d4ba4be3ad07674785e48c402112f25d
jdacewicz <jakub.dacewicz99@gmail.com>
2023-03-03 15:20:08 +0100

    Project created.

---
+++ src/main/java/com/sharememories/sharememories/ShareMemoriesApplication.java
@@ -0,0 +1,13 @@
+package com.sharememories.sharememories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMemoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareMemoriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sharememories/sharememories/ShareMemoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharememories.sharememories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMemoriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
