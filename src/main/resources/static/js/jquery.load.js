1b173c5552e04dd58fae1846eed908361222cbdd
jdacewicz <jakub.dacewicz99@gmail.com>
2023-03-06 12:00:30 +0100

    Implemented basic show posts by API.

---
+++ src/main/resources/static/js/jquery.load.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    loadPosts();
+    $("#posts").fadeIn("slow");
+});
+
+function loadPosts() {
+    $.ajax({
+       type: "GET",
+       url: "/api/posts/random",
+       dataType: "JSON",
+       success: function (data) {
+           data.forEach(function (post) {
+              showPost(post);
+           });
+       }
+    });
+}
+
+function showPost(data) {
+    var index = data.id;
+    $("#posts").append(
+        '<div name="post[' + index + ']" class="max-w-md mx-auto rounded-xl mb-4 bg-white p-4 grid grid-flow-row auto-rows-max shadow">' +
+            '<div class="mt-2">' +
+                '<span name="post[' + index + '].content">' + data.content + '</span>' +
+            '</div>' +
+        '</div>'
+    );
+}
