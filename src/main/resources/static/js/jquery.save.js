1917d0377d5ef917c725a8b7d1f5065005ef7b41
jdacewicz <jakub.dacewicz99@gmail.com>
2023-03-07 09:59:00 +0100

    Added creating Reaction form.

--- src/main/resources/static/js/jquery.save.js
+++ src/main/resources/static/js/jquery.save.js
@@ -1,7 +1,25 @@
 $(function () {
+    $("#show-reaction-panel").click(function () {
+        $("#main-content").hide();
+        $("#reaction-panel").show();
+        $("#panels").fadeIn("slow");
+    });
+
     $("#create-post-form").submit(function (e) {
         e.preventDefault();
         var frm = $("#create-post-form");
+        var data = {};
+        $.each(this, function (i, v) {
+            var input = $(v);
+            data[input.attr("name")] = input.val();
+            delete data["undefined"];
+        });
+        saveRequestedData(frm, data);
+    });
+
+    $("#create-reaction-form").submit(function (e) {
+        e.preventDefault();
+        var frm = $("#create-reaction-form");
         var data = {};
         $.each(this, function (i, v) {
             var input = $(v);
