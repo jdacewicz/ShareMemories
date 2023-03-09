9df1a2540a06f4a4f64d8f0c3d6e6c9fb95e5734
jdacewicz <jakub.dacewicz99@gmail.com>
2023-03-09 15:13:56 +0100

    Implemented displaying defined Reactions and Post reactions counts.

--- src/main/resources/static/js/jquery.boot.js
+++ src/main/resources/static/js/jquery.boot.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $("#posts").fadeIn("slow");
 
     $("#show-reaction-panel").click(function () {
-        loadReactions();
+        loadReactionPanelData();
         $("#main-content").hide();
         $("#reactions").show();
         $("#reactions-create").show();
@@ -11,7 +11,7 @@ $(document).ready(function () {
     });
 
     $("#reactions").on("click", "button[name='reaction-delete']", function () {
-        var id = $(this).val();
+        let id = $(this).val();
         deleteReaction(id);
     });
 });
@@ -21,22 +21,35 @@ function loadPosts() {
        type: "GET",
        url: "/api/posts/random",
        dataType: "JSON",
-       success: function (data) {
-           data.forEach(function (post) {
-              showPost(post);
+       success: function (posts) {
+           $.ajax({
+               type: "GET",
+               url: "/api/reactions",
+               dataType: "JSON",
+               success: function (reactions) {
+                   posts.forEach(function (post) {
+                       appendPost(post);
+                   });
+                   reactions.forEach(function (reaction) {
+                       appendReaction(reaction);
+                   });
+                   posts.forEach(function (post) {
+                       setPostReactionCount(post);
+                   });
+               }
            });
        }
     });
 }
 
-function loadReactions() {
+function loadReactionPanelData() {
     $.ajax({
        type: "GET",
        url: "/api/reactions",
        dataType: "JSON",
        success: function (data) {
            data.forEach(function (reaction) {
-               showReaction(reaction);
+               appendReactionDataToPanel(reaction);
            });
        }
     });
@@ -52,24 +65,45 @@ function deleteReaction(id) {
     });
 }
 
-function showPost(data) {
-    var index = data.id;
+function appendPost(post) {
+    let index = post.id;
     $("#posts").append(
         '<div name="post[' + index + ']" class="max-w-md mx-auto rounded-xl mb-4 bg-white p-4 grid grid-flow-row auto-rows-max shadow">' +
-            '<div class="float-right"><span>' + data.elapsedCreationTimeMessage + '</span></div>' +
+            '<div class="float-right"><span>' + post.elapsedCreationTimeMessage + '</span></div>' +
             '<div class="mt-2">' +
-                '<span>' + data.content + '</span>' +
+                '<span>' + post.content + '</span>' +
+            '</div>' +
+            '<div name="reactions">' +
             '</div>' +
         '</div>'
     );
 }
 
-function showReaction(data) {
-    var index = data.id;
+function appendReaction(reaction) {
+    let index = reaction.id;
+    $("div[name='reactions']").append(
+        '<div name="reaction[' + index + ']">' +
+            '<button type="button" value="' +  index + '">' +
+                '<span></span>' +
+                '<img src="' + reaction.image + '">' +
+            '</button>' +
+        '</div>'
+    );
+}
+
+function setPostReactionCount(post) {
+    let index = post.id;
+    $.each(post.reactionsCounts ,function (key, value) {
+        $("div[name='post[" + index + "]'] div[name='reactions'] div[name='reaction[" + key + "]'] span").text(value);
+    });
+}
+
+function appendReactionDataToPanel(reaction) {
+    let index = reaction.id;
     $("#reactions tbody").append(
         '<tr name="reaction[' + index + ']" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">' +
-            '<th scope="row" class="px-6 py-4">' + data.image + '</th>' +
-            '<td class="px-6 py-4">' + data.name + '</td>' +
+            '<th scope="row" class="px-6 py-4">' + reaction.image + '</th>' +
+            '<td class="px-6 py-4">' + reaction.name + '</td>' +
             '<td class="px-6 py-4">' +
                 '<button type="button" value="' + index + '" name="reaction-edit">EDIT</button>' +
                 ' ' +
