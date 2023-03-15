4461df88c98fc3b619aa76ff0f9e9484a3daf73d
jdacewicz <jakub.dacewicz99@gmail.com>
2023-03-15 18:29:57 +0100

    Sending file instead of text in Reaction create - fix

--- src/main/resources/static/js/jquery.save.js
+++ src/main/resources/static/js/jquery.save.js
@@ -9,29 +9,32 @@ $(function () {
 
     $("#create-reaction-form").submit(function (e) {
         e.preventDefault();
-        var frm = $("#create-reaction-form");
-        var data = {};
-        $.each(this, function (i, v) {
-            var input = $(v);
-            data[input.attr("name")] = input.val();
-            delete data["undefined"];
-        });
-        saveRequestedData(frm, data);
+        let frm = $("#create-reaction-form");
+        // var data = {};
+        // $.each(this, function (i, v) {
+        //     var input = $(v);
+        //     data[input.attr("name")] = input.val();
+        //     delete data["undefined"];
+        // });
+        // saveRequestedData(frm, data);
+        let data = new FormData($('#create-reaction-form')[0]);
+
+        saveMultipartRequestedData(frm, data)
     });
 });
 
-function saveRequestedData(frm, data) {
-    $.ajax({
-        contentType: "application/json; charset=utf-8",
-        url: frm.attr("action"),
-        type: frm.attr("method"),
-        dataType: "json",
-        data: JSON.stringify(data),
-        success: function () {
-            location.reload();
-        }
-    });
-}
+// function saveRequestedData(frm, data) {
+//     $.ajax({
+//         contentType: "application/json; charset=utf-8",
+//         url: frm.attr("action"),
+//         type: frm.attr("method"),
+//         dataType: "json",
+//         data: JSON.stringify(data),
+//         success: function () {
+//             location.reload();
+//         }
+//     });
+// }
 
 function saveMultipartRequestedData(frm, data) {
     $.ajax({
