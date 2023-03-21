0d6e0fc63a482822ee707c9279f05e6b3de8a5d5
jdacewicz <jakub.dacewicz99@gmail.com>
2023-03-21 15:45:28 +0100

    Failed to load images - fix

--- src/main/java/com/sharememories/sharememories/config/MvcConfig.java
+++ src/main/java/com/sharememories/sharememories/config/MvcConfig.java
@@ -1,11 +1,13 @@
 package com.sharememories.sharememories.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
