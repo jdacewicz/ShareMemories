bb1aa3c066fbc02c4042c22fe267b50b40d6b8cf
jdacewicz <jakub.dacewicz99@gmail.com>
2023-03-29 13:42:18 +0200

    Created User entity.

---
+++ src/main/java/com/sharememories/sharememories/domain/User.java
@@ -0,0 +1,64 @@
+package com.sharememories.sharememories.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "userId")
+    private Long id;
+    @JsonIgnore
+    private String username;
+    @JsonIgnore
+    private String password;
+    private boolean accountNonLocked = true;
+    @JsonIgnore
+    private String role = "ROLE_USER";
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
