48c3a38a707bda0d5cb304fa6859d7913000ce1c
jdacewicz <jakub.dacewicz99@gmail.com>
2023-04-13 16:23:19 +0200

    Tests for MessageService.

---
+++ src/test/java/com/sharememories/sharememories/service/MessageServiceTest.java
@@ -0,0 +1,62 @@
+package com.sharememories.sharememories.service;
+
+import com.sharememories.sharememories.domain.Message;
+import com.sharememories.sharememories.domain.User;
+import com.sharememories.sharememories.repository.MessageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageServiceTest {
+
+    @InjectMocks
+    private MessageService service;
+    @Mock
+    private MessageRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void Given_User_When_GettingNotificationsCountByMessageReceiverReturnsNotEmptyList_Then_ReturnedProperMapOfCounts() {
+        //Given
+        User receiver = new User();
+        receiver.setId((long) 1);
+        //When
+        User sender = new User();
+        sender.setId((long) 2);
+        Message message = new Message(sender, receiver, "test");
+        List<Message> messageList = List.of(message, message);
+
+        Mockito.when(repository.getAllByReceiverAndMessageSeen(receiver, false)).thenReturn(messageList);
+
+        Map<Long, Long> output = service.getNotificationsCount(receiver);
+        //Then
+        assertEquals(2, output.get((long) 2));
+    }
+
+    @Test
+    void Given_User_When_GettingNotificationsCountByMessageReceiverReturnsEmptyList_Then_ReturnedEmptyMapOfCounts() {
+        //Given
+        User receiver = new User();
+        receiver.setId((long) 1);
+        //When
+        List<Message> messageList = List.of();
+
+        Mockito.when(repository.getAllByReceiverAndMessageSeen(receiver, false)).thenReturn(messageList);
+
+        Map<Long, Long> output = service.getNotificationsCount(receiver);
+        //Then
+        assert(output.isEmpty());
+    }
+}
