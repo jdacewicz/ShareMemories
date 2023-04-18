fda029d31c128291024a1fc0b561c2a8810444f9
jdacewicz <jakub.dacewicz99@gmail.com>
2023-04-18 18:09:04 +0200

    Tests for MessageService - c. d.

--- src/test/java/com/sharememories/sharememories/service/MessageServiceTest.java
+++ src/test/java/com/sharememories/sharememories/service/MessageServiceTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class MessageServiceTest {
 
@@ -28,7 +28,7 @@ class MessageServiceTest {
     }
 
     @Test
-    void Given_User_When_GettingNotificationsCountByMessageReceiver_Then_ReturnedProperMapOfCountsOfMessagesFromContacts() {
+    void Given_User_When_GettingNotificationsCountByProperMessageReceiver_Then_ReturnedProperMapOfCountsOfMessagesFromContacts() {
         //Given
         User receiver = new User();
         receiver.setId((long) 1);
@@ -40,7 +40,7 @@ class MessageServiceTest {
         Message message = new Message(sender, receiver, "test");
         List<Message> messageList = List.of(message, message);
 
-        Mockito.when(repository.getAllByReceiverAndMessageSeen(receiver, false)).thenReturn(messageList);
+        when(repository.getAllByReceiverAndMessageSeen(receiver, false)).thenReturn(messageList);
 
         Map<Long, Long> output = service.getAllNotificationsCount(receiver);
         //Then
@@ -48,7 +48,7 @@ class MessageServiceTest {
     }
 
     @Test
-    void Given_User_When_GettingNotificationsCountByMessageReceiver_Then_ReturnedProperMapOfCountsOfMessagesNotFromContacts() {
+    void Given_User_When_GettingNotificationsCountByProperMessageReceiver_Then_ReturnedProperMapOfCountsOfMessagesNotFromContacts() {
         //Given
         User receiver = new User();
         receiver.setId((long) 1);
@@ -59,10 +59,49 @@ class MessageServiceTest {
         Message message = new Message(sender, receiver, "test");
         List<Message> messageList = List.of(message, message);
 
-        Mockito.when(repository.getAllByReceiverAndMessageSeen(receiver, false)).thenReturn(messageList);
+        when(repository.getAllByReceiverAndMessageSeen(receiver, false)).thenReturn(messageList);
 
         Map<Long, Long> output = service.getAllNotificationsCount(receiver);
         //Then
         assertEquals(2, output.get((long) -1));
+    }
+
+    @Test
+    void Given_User_When_GettingNotificationsCountByWrongMessageReceiver_Then_ReturnedEmptyMap() {
+        //Given
+        User receiver = new User();
+        //When
+        Map<Long, Long> output = service.getAllNotificationsCount(receiver);
+        //Then
+        assert(output.isEmpty());
+    }
+
+    @Test
+    void Given_User_When_GettingUnknownSenderNotificationsCountByProperMessageReceiver_Then_ReturnedProperMapOfCountsOfMessages() {
+        //Given
+        User receiver = new User();
+        //When
+        User sender = new User();
+        sender.setId((long) 1);
+
+        Message message = new Message(sender, receiver, "test");
+        List<Message> messageList = List.of(message, message);
+
+        when(repository.findAllByReceiverAndSenderNotInContactsAndMessageSeen(receiver, receiver.getContacts(), false))
+                .thenReturn(messageList);
+
+        Map<Long, Long> output = service.getUnknownSenderNotificationsCount(receiver);
+        //Then
+        assertFalse(output.isEmpty());
+    }
+
+    @Test
+    void Given_User_When_GettingUnknownSenderNotificationsCountByWrongMessageReceiver_Then_ReturnedEmptyMap() {
+        //Given
+        User receiver = new User();
+        //When
+        Map<Long, Long> output = service.getUnknownSenderNotificationsCount(receiver);
+        //Then
+        assert(output.isEmpty());
     }
 }
