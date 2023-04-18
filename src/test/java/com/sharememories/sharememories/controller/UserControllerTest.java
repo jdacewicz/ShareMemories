feaee29edbc06f045f50fc314823cf4d73563458
jdacewicz <jakub.dacewicz99@gmail.com>
2023-04-18 19:35:58 +0200

    Tests for UserController.

--- src/test/java/com/sharememories/sharememories/controller/UserControllerTest.java
+++ src/test/java/com/sharememories/sharememories/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.sharememories.sharememories.controller;
 import com.sharememories.sharememories.domain.User;
 import com.sharememories.sharememories.service.SecurityUserDetailsService;
 import com.sharememories.sharememories.util.FileUtils;
+import org.apache.coyote.Response;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,11 +11,17 @@ import org.junit.jupiter.api.Test;
 import org.mockito.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class UserControllerTest {
 
@@ -88,5 +95,47 @@ class UserControllerTest {
         ResponseEntity response = controller.deleteUser(id);
         //Then
         assertEquals(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build().getStatusCode(), response.getStatusCode());
+    }
+
+    @Test
+    void Given_UserId_When_AddingUserToFriendsByProperAddedUserId_Then_ReturnedResponseOkWithUserLogged() {
+        //Given
+        long addedUserId = 1;
+        //When
+        User loggedUser = new User("user");
+        User addedUser = new User("user2");
+
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(securityContext.getAuthentication().getName()).thenReturn(loggedUser.getUsername());
+        when(service.getUserByUsername(any(String.class))).thenReturn(Optional.of(loggedUser));
+        when(service.addUserToFriendsList(loggedUser, addedUserId)).thenReturn(Optional.of(loggedUser));
+
+        ResponseEntity response = controller.addUserToFriends(addedUserId);
+        //Then
+        assertEquals(ResponseEntity.ok(loggedUser), response);
+    }
+
+    @Test
+    void Given_UserId_When_AddingUserToFriendsByWrongAddedUserId_Then_ReturnedResponseNotFound() {
+        //Given
+        long addedUserId = 1;
+        //When
+        User loggedUser = new User("user");
+
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(securityContext.getAuthentication().getName()).thenReturn(loggedUser.getUsername());
+        when(service.getUserByUsername(any(String.class))).thenReturn(Optional.of(loggedUser));
+
+        ResponseEntity response = controller.addUserToFriends(addedUserId);
+        //Then
+        assertEquals(ResponseEntity.status(HttpStatus.NOT_FOUND).build().getStatusCode(), response.getStatusCode());
     }
 }
