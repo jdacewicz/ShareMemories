4a6d98d3fd5d169d6f898e118d9cb87111172dbe
jdacewicz <jakub.dacewicz99@gmail.com>
2023-05-12 20:11:05 +0200

    Constructor change + small fixes

--- src/main/java/com/sharememories/sharememories/service/EmailServiceImpl.java
+++ src/main/java/com/sharememories/sharememories/service/EmailServiceImpl.java
@@ -1,19 +1,17 @@
 package com.sharememories.sharememories.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class EmailServiceImpl implements EmailService{
 
     private final JavaMailSender mailSender;
 
-    @Autowired
-    public EmailServiceImpl(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
 
     @Override
     public void sendMessage(String from, String to, String subject, String text) {
