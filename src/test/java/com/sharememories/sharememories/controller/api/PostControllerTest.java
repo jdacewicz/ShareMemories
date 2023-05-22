3a03a4aacd32f0a2699afec8a35667b0578f1d09
jdacewicz <jakub.dacewicz99@gmail.com>
2023-05-22 14:48:48 +0200

    Rewriting tests for PostController - c.d.

--- src/test/java/com/sharememories/sharememories/controller/api/PostControllerTest.java
+++ src/test/java/com/sharememories/sharememories/controller/api/PostControllerTest.java
@@ -1,230 +1,305 @@
 package com.sharememories.sharememories.controller.api;
 
-import com.sharememories.sharememories.controller.api.PostController;
 import com.sharememories.sharememories.domain.Post;
 import com.sharememories.sharememories.domain.User;
 import com.sharememories.sharememories.service.PostService;
 import com.sharememories.sharememories.service.SecurityUserDetailsService;
 import com.sharememories.sharememories.util.FileUtils;
+import com.sharememories.sharememories.util.UserUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.mock.web.MockPart;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(PostController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class PostControllerTest {
 
-    @InjectMocks
-    private PostController controller;
-    @Mock
-    private PostService postService;
-    @Mock
-    SecurityUserDetailsService detailsService;
-    static MockedStatic<FileUtils> fileUtils;
+    @Autowired
+    private MockMvc mvc;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+    @MockBean
+    private PostService postService;
+
+    @MockBean
+    private SecurityUserDetailsService detailsService;
+
+    private static MockedStatic<FileUtils> fileUtils;
+    private static MockedStatic<UserUtils> userUtils;
 
     @BeforeAll
     static void init() {
         fileUtils = Mockito.mockStatic(FileUtils.class);
+        userUtils = Mockito.mockStatic(UserUtils.class);
     }
 
     @AfterAll
     static void close() {
         fileUtils.close();
+        userUtils.close();
     }
 
     @Test
-    void Given_Id_When_GettingPostByIdByAPIReturnsPost_Then_ReturnedResponseOkWithPost() {
-        //Given
+    @DisplayName("Given valid post id " +
+            "When getting existing post by api " +
+            "Then should return ok")
+    void getExistingPostByValidId() throws Exception {
         long id = 1;
-        //When
-        Post post = new Post();
-        Mockito.when(postService.getPost(id)).thenReturn(Optional.of(post));
 
-        ResponseEntity response = controller.getPost(id);
-        //Then
-        assertEquals(ResponseEntity.ok(post), response);
+        when(postService.getPost(id)).thenReturn(Optional.of(new Post()));
+
+        this.mvc.perform( get("/api/posts/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void Given_Id_When_GettingPostByIdByAPINotReturnsPost_Then_ReturnedResponseNotFound() {
-        //Given
+    @DisplayName("Given valid post id " +
+            "When getting non existing post by api " +
+            "Then should return not found")
+    void getNonExistingPostByValidId() throws Exception {
         long id = 1;
-        //When
-        ResponseEntity response = controller.getPost(id);
-        //Then
-        assertEquals(ResponseEntity.status(HttpStatus.NOT_FOUND).build().getStatusCode(), response.getStatusCode());
+
+        this.mvc.perform( get("/api/posts/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    void Given__When_GettingRandomPostsByAPIReturnsEmptyList_Then_ReturnedResponseNoContent() {
-        //Given
-        //When
-        Mockito.when(postService.getRandomPosts()).thenReturn(List.of());
+    @DisplayName("Given invalid post id " +
+            "When getting post by api " +
+            "Then should return bad request")
+    void getPostByInvalidId() throws Exception {
+        float id = 1.2f;
 
-        ResponseEntity response = controller.getRandomPosts();
-        //Then
-        assertEquals(ResponseEntity.status(HttpStatus.NO_CONTENT).build(), response);
+        this.mvc.perform( get("/api/posts/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    void Given__When_GettingRandomPostsByAPIReturnsNotEmptyList_Then_ReturnedResponseOKWithList() {
-        //Given
-        //When
-        Post post = new Post();
-        List<Post> posts = List.of(post);
-        Mockito.when(postService.getRandomPosts()).thenReturn(posts);
+    @DisplayName("Given " +
+            "When getting random posts by api returns not empty list " +
+            "Then should return ok")
+    void getNotEmptyRandomPosts() throws Exception {
+        long id = 1;
 
-        ResponseEntity response = controller.getRandomPosts();
-        //Then
-        assertEquals(ResponseEntity.ok(posts), response);
+        when(postService.getRandomPosts()).thenReturn(List.of(new Post()));
+
+        this.mvc.perform( get("/api/posts/random", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void Given_Content_When_CreatingPostByAPI_Then_ReturnedResponseCreatedWithPost() {
-        //Given
-        String content = "content";
-        MockMultipartFile file = new MockMultipartFile("image.png", "content".getBytes());
-        //When
-        User user = new User("user");
-        Post post = new Post(content, file.getOriginalFilename(), user);
+    @DisplayName("Given " +
+            "When getting random posts by api returns empty list " +
+            "Then should return no content")
+    void getEmptyRandomPosts() throws Exception {
+        long id = 1;
 
-        Authentication authentication = Mockito.mock(Authentication.class);
-        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
-        SecurityContextHolder.setContext(securityContext);
-
-        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
-        Mockito.when(securityContext.getAuthentication().getName()).thenReturn(user.getUsername());
-        Mockito.when(detailsService.getUserByUsername(any(String.class))).thenReturn(Optional.of(user));
-        Mockito.when(postService.createPost(any(Post.class))).thenReturn(post);
-
-        ResponseEntity response = controller.createPost(content, file);
-        //Then
-        assertEquals(ResponseEntity.status(HttpStatus.CREATED).body(post), response);
+        this.mvc.perform( get("/api/posts/random", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
     }
 
     @Test
-    void Given_ContentAndFile_When_CreatingPostByAPI_Then_ReturnedResponseCreatedWithPost() {
-        //Given
-        String content = "content";
-        MockMultipartFile file = new MockMultipartFile("name", null, null, new byte[0]);
-        //When
-        User user = new User("user");
-        Post post = new Post(content, user);
+    @DisplayName("Given valid user id " +
+            "When getting all posts by api returns not empty list " +
+            "Then should return ok")
+    void getAllPostsByValidUserIdReturnsNotEmptyList() throws Exception {
+        long id = 1;
 
-        Authentication authentication = Mockito.mock(Authentication.class);
-        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
-        SecurityContextHolder.setContext(securityContext);
+        when(postService.getAllByCreatorId(id)).thenReturn(List.of(new Post()));
 
-        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
-        Mockito.when(securityContext.getAuthentication().getName()).thenReturn(user.getUsername());
-        Mockito.when(detailsService.getUserByUsername(any(String.class))).thenReturn(Optional.of(user));
-        Mockito.when(postService.createPost(any(Post.class))).thenReturn(post);
-
-        ResponseEntity response = controller.createPost(content, file);
-        //Then
-        assertEquals(ResponseEntity.status(HttpStatus.CREATED).body(post), response);
+        this.mvc.perform( get("/api/posts/user/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void Given_ContentAndFile_When_ErrorWhileCreatingPostByAPI_Then_ReturnedResponseInternalServerError() {
-        //Given
-        String content = "content";
-        MockMultipartFile file = new MockMultipartFile("image.png", "content".getBytes());
-        //When
-        User user = new User("user");
+    @DisplayName("Given valid user id " +
+            "When getting all posts by api returns empty list " +
+            "Then should return no content")
+    void getAllPostsByValidUserIdReturnsEmptyList() throws Exception {
+        long id = 1;
 
-        Authentication authentication = Mockito.mock(Authentication.class);
-        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
-        SecurityContextHolder.setContext(securityContext);
-
-        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
-        Mockito.when(securityContext.getAuthentication().getName()).thenReturn(user.getUsername());
-        Mockito.when(detailsService.getUserByUsername(any(String.class))).thenReturn(Optional.of(user));
-        fileUtils.when(() -> FileUtils.generateUniqueName(file.getOriginalFilename())).thenReturn(file.getOriginalFilename());
-        fileUtils.when(() -> FileUtils.saveFile(Post.IMAGES_DIRECTORY_PATH, file.getOriginalFilename(), file)).thenThrow(IOException.class);
-
-        ResponseEntity response = controller.createPost(content, file);
-        //Then
-        assertEquals(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build().getStatusCode(), response.getStatusCode());
+        this.mvc.perform( get("/api/posts/user/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
     }
 
     @Test
-    void Given_PostIdReactionId_When_ReactingToPostByPostIdAndReactionIdByAPI_Then_ReturnedResponseOkWithPost() {
-        //Given
+    @DisplayName("Given invalid user id " +
+            "When getting all posts by api " +
+            "Then should return bad request")
+    void getAllPostsByInvalidUserId() throws Exception {
+        float id = 1.2f;
+
+        this.mvc.perform( get("/api/posts/user/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Given valid content and image " +
+            "When creating post by api " +
+            "Then should return created")
+    void createPostWithValidData() throws Exception {
+        MockPart content = new MockPart("content", "content".getBytes());
+        MockMultipartFile image = new MockMultipartFile("image" , "".getBytes());
+
+        userUtils.when(() -> UserUtils.getLoggedUser(any(SecurityUserDetailsService.class))).thenReturn(new User());
+        fileUtils.when(() -> FileUtils.saveFile(any(), any(), any())).thenThrow(IOException.class);
+        when(postService.createPost(any(Post.class))).thenReturn(new Post());
+
+        this.mvc.perform( multipart("/api/posts")
+                        .file(image)
+                        .part(content))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("Given content and image " +
+            "When creating post by api throws file error " +
+            "Then should return internal server error")
+    void createPostWithFileError() throws Exception {
+        MockPart content = new MockPart("content", "content".getBytes());
+        MockMultipartFile image = new MockMultipartFile("image", "file.png", "image/png" , "file".getBytes());
+
+        userUtils.when(() -> UserUtils.getLoggedUser(any(SecurityUserDetailsService.class))).thenReturn(new User());
+        fileUtils.when(() -> FileUtils.saveFile(any(), any(), any())).thenThrow(IOException.class);
+        when(postService.createPost(any(Post.class))).thenReturn(new Post());
+
+        this.mvc.perform( multipart("/api/posts")
+                        .file(image)
+                        .part(content))
+                .andDo(print())
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    @DisplayName("Given valid post and reaction ids " +
+            "When reacting to existing post by api " +
+            "Then should return ok")
+    void reactToExistingPostByItsAndReactionValidId() throws Exception {
         long postId = 1;
         int reactionId = 1;
-        //When
-        Post post = new Post();
-        Mockito.when(postService.reactToPost(reactionId, postId)).thenReturn(Optional.of(post));
 
-        ResponseEntity response = controller.reactToPost(reactionId, postId);
-        //Then
-        assertEquals(ResponseEntity.ok(post), response);
+        when(postService.reactToPost(reactionId, postId)).thenReturn(Optional.of(new Post()));
+
+        this.mvc.perform( put("/api/posts/{postId}/react/{reactionId}", postId, reactionId))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void Given_PostIdReactionId_When_ReactingToPostByWrongPostIdOrReactionIdByAPI_Then_ReturnedResponseOkWithPost() {
-        //Given
+    @DisplayName("Given valid post and reaction ids " +
+            "When reacting to existing post by api " +
+            "Then should return ok")
+    void reactToNonExistingPostByItsAndReactionValidId() throws Exception {
         long postId = 1;
         int reactionId = 1;
-        //When
-        ResponseEntity response = controller.reactToPost(reactionId, postId);
-        //Then
-        assertEquals(ResponseEntity.status(HttpStatus.NOT_FOUND).build().getStatusCode(), response.getStatusCode());
+
+        this.mvc.perform( put("/api/posts/{postId}/react/{reactionId}", postId, reactionId))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    void Given_Id_When_DeletingPostWithoutImageByIdByAPI_Then_ReturnedResponseOk() {
-        //Given
-        long id = 1;
-        //When
-        ResponseEntity response = controller.deletePost(id);
-        //Then
-        assertEquals(ResponseEntity.ok().build(), response);
+    @DisplayName("Given invalid post and reaction ids " +
+            "When reacting to post by api " +
+            "Then should return bad request")
+    void reactToPostByItsAndReactionInvalidId() throws Exception {
+        float postId = 1.2f;
+        float reactionId = 1.2f;
+
+        this.mvc.perform( put("/api/posts/{postId}/react/{reactionId}", postId, reactionId))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    void Given_Id_When_DeletingPostWithImageByIdByAPI_Then_ReturnedResponseOk() {
-        //Given
+    @DisplayName("Given valid post id " +
+            "When deleting existing post by api " +
+            "Then should return ok")
+    void deleteExistingPostByItsValidId() throws Exception {
         long id = 1;
-        //When
-        String imageName = "name";
-        Mockito.when(postService.getPostImageName(id)).thenReturn(Optional.of(imageName));
 
-        ResponseEntity response = controller.deletePost(id);
-        //Then
-        assertEquals(ResponseEntity.ok().build(), response);
+        when(postService.getPost(id)).thenReturn(Optional.of(new Post()));
+
+        this.mvc.perform( delete("/api/posts/{id}", id))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void Given_Id_When_DeletingPostWithImageByWrongIdByAPI_Then_ReturnedResponseInternalServerError() {
-        //Given
+    @DisplayName("Given valid post id " +
+            "When deleting non existing post by api " +
+            "Then should return not found")
+    void deleteNonExistingPostByValidId() throws Exception {
         long id = 1;
-        //When
-        String imageName = "name";
-        Mockito.when(postService.getPostImageName(id)).thenReturn(Optional.of(imageName));
-        fileUtils.when(() -> FileUtils.deleteFile(Post.IMAGES_DIRECTORY_PATH, imageName)).thenThrow(IOException.class);
 
-        ResponseEntity response = controller.deletePost(id);
-        //Then
-        assertEquals(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build().getStatusCode(), response.getStatusCode());
+        this.mvc.perform( delete("/api/posts/{id}", id))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Given invalid post id " +
+            "When deleting post by api " +
+            "Then should return bad request")
+    void deletePostByInvalidId() throws Exception {
+        float id = 1.2f;
+
+        this.mvc.perform( delete("/api/posts/{id}", id))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Given valid post id " +
+            "When deleting existing post by api throws file error" +
+            "Then should return internal server error")
+    void deleteExistingPostByValidIdThrowsFileError() throws Exception {
+        long id = 1;
+
+        Post post = new Post();
+        post.setImage("image.png");
+        when(postService.getPost(id)).thenReturn(Optional.of(post));
+        fileUtils.when(() -> FileUtils.deleteFile(any(), any())).thenThrow(IOException.class);
+
+        this.mvc.perform( delete("/api/posts/{id}", id))
+                .andDo(print())
+                .andExpect(status().isInternalServerError());
     }
 }
